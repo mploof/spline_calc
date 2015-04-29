519eca8f6027d5bdcba5167501fb750c3795bc6b
Michael Ploof <michael@dynamicperception.com>
2015-04-29 09:25:21 -0400

    Float calculation of B-spline control points

--- spline_calc.cpp
+++ spline_calc.cpp
@@ -18,25 +18,25 @@ Spline::Spline() {
 }
 
 
-void Spline::setInterpPts(int* p_points, int p_count){
+void Spline::setInterpPts(float* p_points, int p_count){
 
 	// Initialize the interpolation points matrix
 	m_interp_pts.init(p_count, 2);
 	
-	// Set the 1D array as the new interpolation point matrix
+	// Set the 1D array as the new interpolation pofloat matrix
 	m_interp_pts.setValues(p_points, p_count * 2);
-
+	Serial.println("Solving b-spline");
 	// Solve for the B-spline control points
 	solveBsplineCtlPnts();
-
+	Serial.println("Solving beziers");
 	// Solve for the Bezier control points
 	solveBezCtrlPts();
 }
 
 void Spline::solveBezCtrlPts(){
 
-	const int XY_COL = 2;			// Number of values to represent an XY point
-	const int BEZ_PNT_CNT = 4;		// Number of control points required to define a cubic Bezier curve
+	const float XY_COL = 2;			// Number of values to represent an XY point
+	const float BEZ_PNT_CNT = 4;		// Number of control points required to define a cubic Bezier curve
 
 	// Determine how many curves comprise the spline
 	m_bez_count = m_b_ctrl_pts.rowCount() - 1;
@@ -51,7 +51,7 @@ void Spline::solveBezCtrlPts(){
 		m_bez_ctrl_pts[i].init(BEZ_PNT_CNT, XY_COL);
 	}
 
-	//m_b_ctrl_pts.print("B Spline Ctrl Points", false);
+	//m_b_ctrl_pts.print("B Spline Ctrl Points");
 
 	// Populate the Bezier control points
 	for (byte i = 0; i < m_bez_count; i++){
@@ -66,13 +66,13 @@ void Spline::solveBezCtrlPts(){
 					}
 					case 1:
 					{
-						int pnt = (float)m_b_ctrl_pts.getValue(i, c) * 2 / 3 + (float)m_b_ctrl_pts.getValue(i + 1, c) * 1 / 3;
+						float pnt = (float)m_b_ctrl_pts.getValue(i, c) * 2 / 3 + (float)m_b_ctrl_pts.getValue(i + 1, c) * 1 / 3;
 						m_bez_ctrl_pts[i].setValue(r, c, pnt);
 						break;
 					}
 					case 2:
 					{
-						int pnt = (float)m_b_ctrl_pts.getValue(i, c) * 1 / 3 + (float)m_b_ctrl_pts.getValue(i + 1, c) * 2 / 3;
+						float pnt = (float)m_b_ctrl_pts.getValue(i, c) * 1 / 3 + (float)m_b_ctrl_pts.getValue(i + 1, c) * 2 / 3;
 						m_bez_ctrl_pts[i].setValue(r, c, pnt);
 						break;
 					}
@@ -84,7 +84,7 @@ void Spline::solveBezCtrlPts(){
 				}
 			}
 		}
-		//m_bez_ctrl_pts[i].print("Bez ctrl", false);
+		//m_bez_ctrl_pts[i].print("Bez ctrl");
 	}
 }
 
@@ -93,18 +93,16 @@ void Spline::getCurvePts(int p_point_count){
 	
 	//Serial.println("Getting curve points");
 
-	const int XY_COL = 2;											// Number of values to represent an XY point
-	const int BEZ_PNT_CNT = 4;										// Number of control points required to define a cubic Bezier curve
 
 	// If the output matrix isn't the right size, reinitialize it
 	if (m_curve_pts.rowCount() != p_point_count || m_curve_pts.colCount() != XY_COL)
 		m_curve_pts.init(p_point_count, XY_COL);
 
-	float increment = (float)m_bez_count / (float)p_point_count;	// Amount by which t should increment
-	int which = 0;													// Current curve being operated upon
-	float t = 0;													// Current t value
+	float increment = (float)m_bez_count / (float)(p_point_count - 1);	// Amount by which t should increment
+	int which = 0;														// Current curve being operated upon
+	float t = 0;														// Current t value
 
-	for (int r = 0; r < p_point_count; r++){
+	for (float r = 0; r < p_point_count; r++){
 		
 		// If this is the last point, force t to be the 
 		// end of the curve so we get an accurate end point
@@ -129,20 +127,24 @@ void Spline::getCurvePts(int p_point_count){
 	}
 }
 
-void Spline::getAccel(int p_t){
+void Spline::getAccel(float p_t){
 
 }
 
 // Finds B-spline control points
 void Spline::solveBsplineCtlPnts(){
 
-	// Create a 141 matrix with order interpolation point count - 2
+	float inv_1_row = 0.25;
+
+	// Create a 141 matrix with order interpolation pofloat count - 2
 	matrix diag141(m_interp_pts.rowCount() - 2, m_interp_pts.rowCount() - 2);
 	diag141.set141();
 	
 	// Find inverse of 141 matrix
 	matrix inv_diag141;
-	matrix::inverse(diag141, inv_diag141);
+	if (diag141.rowCount() > 1){
+		matrix::inverse(diag141, inv_diag141);
+	}
 
 	// Create a matrix to hold the constants for solving the control points
 	matrix constants(m_interp_pts.rowCount() - 2, 2);
@@ -167,8 +169,11 @@ void Spline::solveBsplineCtlPnts(){
 
 	// Solve for the control points
 	matrix temp_ctrl_pts;
-	matrix::mult(inv_diag141, constants, temp_ctrl_pts);
-
+	if (diag141.rowCount() > 1)
+		matrix::mult(inv_diag141, constants, temp_ctrl_pts);
+	else
+		matrix::mult(inv_1_row, constants, temp_ctrl_pts);
+	
 	// Save to control points matrix, including first and last interpolation points
 	m_b_ctrl_pts.init(m_interp_pts.rowCount(), 2);
 	for (byte r = 0; r < m_b_ctrl_pts.rowCount(); r++){
@@ -179,20 +184,27 @@ void Spline::solveBsplineCtlPnts(){
 				m_b_ctrl_pts.setValue(r, c, temp_ctrl_pts.getValue(r - 1, c));
 		}
 	}
+	m_b_ctrl_pts.print("Control points: ");
+	Serial.println("Exiting b-spline solving");
 }
 
-int Spline::whichCurve(){
-
+void Spline::printInterpPts(bool p_monitor){
+	if (p_monitor)
+		m_interp_pts.print("Interpolation points");
+	else
+		m_interp_pts.print();
 }
 
-void Spline::printInterpPts(){
-	m_interp_pts.print(true);
+void Spline::printCtrlPts(bool p_monitor){
+	if (p_monitor)
+		m_b_ctrl_pts.print("Control points");
+	else
+		m_b_ctrl_pts.print();
 }
 
-void Spline::printCtrlPts(){
-	m_b_ctrl_pts.print(true);
-}
-
-void Spline::printCurvePts(){
-	m_curve_pts.print(true);
+void Spline::printCurvePts(bool p_monitor){
+	if (p_monitor)
+		m_curve_pts.print("Curve points");
+	else
+		m_curve_pts.print();
 }
--- spline_calc.h
+++ spline_calc.h
@@ -9,24 +9,23 @@ class Spline
 	public:
 		Spline();
 		void init();
-		void setInterpPts(int* p_points, int p_count);
+		void setInterpPts(float* p_points, int p_count);
 		void getCurvePts(int p_point_count);
-		void getVel(int p_t);
-		void getAccel(int p_t);
+		void getVel(float p_t);
+		void getAccel(float p_t);
 
-		void printInterpPts();
-		void printCtrlPts();
-		void printCurvePts();
+		void printInterpPts(bool p_monitor);
+		void printCtrlPts(bool p_monitor);
+		void printCurvePts(bool p_monitor);
 
 	private:
 		matrix		m_interp_pts;
 		matrix		m_b_ctrl_pts;
 		matrix*		m_bez_ctrl_pts;
-		floatMatrix m_curve_pts;
+		matrix		m_curve_pts;
 		int			m_bez_count;
 		void		solveBsplineCtlPnts();
 		void		solveBezCtrlPts();
-		int			whichCurve();
 };
 #endif
 
